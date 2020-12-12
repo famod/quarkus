466a8e970429f57d1f12ee1cf2af9cd9993ab189
Falko Modler <famod@users.noreply.github.com>
2020-12-12 22:49:19 +0100

    reclaim output

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -2,6 +2,6 @@
 
 docker rmi node:10 node:12 mcr.microsoft.com/azure-pipelines/node8-typescript:latest
 # That is 18 GB
-sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/share/dotnet &
 # That is 1.2 GB
-sudo rm -rf /usr/share/swift
+sudo rm -rf /usr/share/swift &
